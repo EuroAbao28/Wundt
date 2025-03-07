54e386ecc49213e1235da92d1aae0bbbe5dd76a2
Euro Abao <abaoeuro2002@gmail.com>
2025-03-07 23:58:34 +0800

    gg

--- client/src/components/main_page/ServicesSection.jsx
+++ client/src/components/main_page/ServicesSection.jsx
@@ -10,91 +10,64 @@ import {
 import SectionHeader from "../SectionHeader";
 
 function ServicesSection() {
+  const contents = [
+    {
+      icon: <TbHeartRateMonitor />,
+      title: "Psychological & Psychiatric Testing",
+      subtitle: "For Employment, Legal, Clinical, and Academic Purposes",
+      desc: " Comprehensive assessments tailored for employment screenings, academic needs, clinical diagnostics, and legal cases.",
+    },
+    {
+      icon: <TbBrain />,
+      title: "Child & Adolescent Assessments",
+      subtitle: "Supporting Young Minds with Care",
+      desc: "Specialized therapy for behavioral issues, developmental concerns, intellectual evaluations, and emotional quotient assessments.",
+    },
+    {
+      icon: <TbUsersGroup />,
+      title: "Clinical Consultation & Counseling",
+      subtitle: "Personalized Care for Mental Well-being",
+      desc: "Address interpersonal, marital, and trauma-related concerns through professional counseling and interventions.",
+    },
+    {
+      icon: <TbBook />,
+      title: "Tutorial & Review Classes",
+      subtitle: "Guiding Future Professionals",
+      desc: "Prepare for PRC Board Examinations with expert-led tutorials in psychology, psychometrics, and education.",
+    },
+    {
+      icon: <TbClipboardList />,
+      title: "Program Development & Administration",
+      subtitle: "For Students, Employees, and Organizations",
+      desc: "Tailored programs fostering growth through psychological insights and strategic development plans.",
+    },
+    {
+      icon: <TbPresentation />,
+      title: "Trainings & Workshops",
+      subtitle: "Building Stronger Teams and Communities",
+      desc: "Engage in seminars, workshops, and team-building activities promoting mental wellness and harmony.",
+    },
+  ];
+
   return (
     <div className="px-6 lg:px-12">
       <div className="mt-40  max-w-7xl mx-auto">
         <SectionHeader title={"Comprehensive Services We Offer"} />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-12">
-          <div className="p-8 rounded flex flex-col items-center text-center text-sm text-slate-500 shadow-card2 transition-all hover:shadow-card duration-500">
-            <TbHeartRateMonitor className="text-4xl text-emerald-600 " />
-            <h3 className="font-semibold text-slate-800 text-lg mt-4">
-              Psychological & Psychiatric Testing
-            </h3>
-            <p className="text-xs italic">
-              For Employment, Legal, Clinical, and Academic Purposes
-            </p>
-            <p className="mt-6">
-              Comprehensive assessments tailored for employment screenings,
-              academic needs, clinical diagnostics, and legal cases.
-            </p>
-          </div>
-
-          <div className="p-8 rounded flex flex-col items-center text-center text-sm text-slate-500 shadow-card2 transition-all hover:shadow-card duration-500">
-            <TbBrain className="text-4xl text-emerald-600 " />
-            <h3 className="font-semibold text-slate-800 text-lg mt-4">
-              Child & Adolescent Assessments
-            </h3>
-            <p className="text-xs italic">Supporting Young Minds with Care</p>
-            <p className="mt-6">
-              Specialized therapy for behavioral issues, developmental concerns,
-              intellectual evaluations, and emotional quotient assessments.
-            </p>
-          </div>
-
-          <div className="p-8 rounded flex flex-col items-center text-center text-sm text-slate-500 shadow-card2 transition-all hover:shadow-card duration-500">
-            <TbUsersGroup className="text-4xl text-emerald-600 " />
-            <h3 className="font-semibold text-slate-800 text-lg mt-4">
-              Clinical Consultation & Counseling
-            </h3>
-            <p className="text-xs italic">
-              Personalized Care for Mental Well-being
-            </p>
-            <p className="mt-6">
-              Address interpersonal, marital, and trauma-related concerns
-              through professional counseling and interventions.
-            </p>
-          </div>
-
-          <div className="p-8 rounded flex flex-col items-center text-center text-sm text-slate-500 shadow-card2 transition-all hover:shadow-card duration-500">
-            <TbBook className="text-4xl text-emerald-600 " />
-            <h3 className="font-semibold text-slate-800 text-lg mt-4">
-              Tutorial & Review Classes
-            </h3>
-            <p className="text-xs italic">Guiding Future Professionals</p>
-            <p className="mt-6">
-              Prepare for PRC Board Examinations with expert-led tutorials in
-              psychology, psychometrics, and education.
-            </p>
-          </div>
-
-          <div className="p-8 rounded flex flex-col items-center text-center text-sm text-slate-500 shadow-card2 transition-all hover:shadow-card duration-500">
-            <TbClipboardList className="text-4xl text-emerald-600 " />
-            <h3 className="font-semibold text-slate-800 text-lg mt-4">
-              Program Development & Administration
-            </h3>
-            <p className="text-xs italic">
-              For Students, Employees, and Organizations
-            </p>
-            <p className="mt-6">
-              Tailored programs fostering growth through psychological insights
-              and strategic development plans.
-            </p>
-          </div>
-
-          <div className="p-8 rounded flex flex-col items-center text-center text-sm text-slate-500 shadow-card2 transition-all hover:shadow-card duration-500">
-            <TbPresentation className="text-4xl text-emerald-600 " />
-            <h3 className="font-semibold text-slate-800 text-lg mt-4">
-              Trainings & Workshops
-            </h3>
-            <p className="text-xs italic">
-              Building Stronger Teams and Communities
-            </p>
-            <p className="mt-6">
-              Engage in seminars, workshops, and team-building activities
-              promoting mental wellness and harmony.
-            </p>
-          </div>
+          {contents.map((content, index) => (
+            <div
+              key={index}
+              className="p-8 rounded flex flex-col items-center text-center text-slate-500 shadow-card2 transition-all hover:shadow-card duration-500"
+            >
+              <p className="text-4xl text-emerald-600">{content.icon}</p>
+              <h3 className="font-semibold text-slate-800 text-base md:text-lg mt-4">
+                {content.title}
+              </h3>
+              <p className="text-xs md:text-sm italic">{content.subtitle}</p>
+              <p className="mt-6 text-xs md:text-sm">{content.desc}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
