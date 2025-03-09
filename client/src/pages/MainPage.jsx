0c1cb29a233917cc99418bbd59bc602bade262bc
Euro Abao <abaoeuro2002@gmail.com>
2025-03-10 02:41:23 +0800

    gg

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -38,14 +38,14 @@ function MainPage() {
       <NewsSection ref={newsRef} /> */}
 
       <div style={{ backgroundImage: `url(${b1})` }} className="mt-40">
-        <div className="p-6 lg:p-20 bg-radial-[at_-35%_15%] from-green-500/80 to-emerald-600/80 to-75% flex flex-col items-center justify-center">
-          <h1 className="text-white text-2xl font-semibold ">
+        <div className="p-6 lg:p-20 bg-radial-[at_-35%_15%] from-green-500/80 to-emerald-600/80 to-75% flex flex-col items-center justify-center text-center">
+          <h1 className="text-white text-xl md:text-2xl font-semibold ">
             Your Path to Growth and Well-Being Starts Here
           </h1>
           <p className="text-white text-sm italic mt-4">
             Psychological, Counseling, and Educational Services Tailored for You
           </p>
-          <button className="bg-white text-emerald-600 text-base py-4 px-6 rounded mt-12">
+          <button className="bg-white text-emerald-600 text-base font-semibold py-4 px-6 rounded mt-12">
             Book an Appointment
           </button>
         </div>
