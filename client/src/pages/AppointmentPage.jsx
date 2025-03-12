5f28debda387b33ea812d6edb96c66771515c869
Euro Abao <abaoeuro2002@gmail.com>
2025-03-13 00:46:58 +0800

    gg

--- client/src/pages/AppointmentPage.jsx
+++ client/src/pages/AppointmentPage.jsx
@@ -290,7 +290,7 @@ function AppointmentPage() {
 
             <button
               type="submit"
-              className="bg-emerald-600 text-white rounded w-fit py-2 px-8 mt-4 font-semibold uppercase"
+              className="bg-emerald-600 text-white rounded w-full sm:w-fit py-2 px-8 mt-4 font-semibold uppercase"
             >
               Submit
             </button>
