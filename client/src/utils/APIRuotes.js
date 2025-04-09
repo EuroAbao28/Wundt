6d81501fe5f2493595a33a7539f2bb5efe653bee
EuroAbao28 <abaoeuro2002@gmail.com>
2025-04-10 07:40:59 +0800

    client route changed to live server

--- client/src/utils/APIRuotes.js
+++ client/src/utils/APIRuotes.js
@@ -1,10 +1,11 @@
 // host
 export const LOCAL = "http://localhost:5000";
+export const LIVE_SERVER = "https://wundt.onrender.com";
 
 // admin
-export const URL_LOGIN = `${LOCAL}/api/admin/login`;
-export const URL_GET_CURRENT_ADMIN = `${LOCAL}/api/admin/current-admin`;
+export const URL_LOGIN = `${LIVE_SERVER}/api/admin/login`;
+export const URL_GET_CURRENT_ADMIN = `${LIVE_SERVER}/api/admin/current-admin`;
 
 // appointment
-export const URL_APPTS = `${LOCAL}/api/appointment`;
-export const URL_APPROVE_APPT = `${LOCAL}/api/appointment/updated-status`;
+export const URL_APPTS = `${LIVE_SERVER}/api/appointment`;
+export const URL_APPROVE_APPT = `${LIVE_SERVER}/api/appointment/updated-status`;
